dcd068b32623d6183b52083b91fd53e4a33cc89b
loligsmile <ms_r@hotmail.de>
2016-02-09 22:40:36 +0100

    Funktioniert besser

--- test.py
+++ test.py
@@ -4,38 +4,44 @@ import sys
 
 class Widget2(qg.QWidget):
 	def __init__(self, parent=None):
-
 		super(Widget2, self).__init__()
 		self._set_layout()
 
 	def _set_layout(self):
-		pass
+		self.le = qg.QLineEdit(self)
+		self.le.show()
+		
+	def _show(self):
+		self.show()
+		self.activateWindow()
+
 
 class Widget(qg.QWidget):
 	def __init__(self, parent=None):
 		super(Widget, self).__init__()
-		
+		self.widget2 = Widget2()
+
 		btn = qg.QPushButton(self)
 		btn.clicked.connect(self._show)
 
 	def _show(self):
-		self.widget2 = Widget2()
-		self.widget2.show()
+		self.widget2._show()
 
 
 class Window(qg.QMainWindow):
 	def __init__(self, parent=None):
 		super(Window, self).__init__()
+		self.widget = Widget()
 		self._set_layout()
 
 	def _set_layout(self):
-		self.widget = Widget()
 		self.setCentralWidget(self.widget)
 		self.resize(700, 400)
 
 	def closeEvent(self, event):
 		event.ignore()
 		app.quit()
+
 
 if __name__ == '__main__':
 	app = qg.QApplication(sys.argv)
